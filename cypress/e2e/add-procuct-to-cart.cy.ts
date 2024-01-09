730b3c55383c75ee598f574ba1a032cdbe740ad2
ecthon <ecthon@gmail.com>
2024-01-09 10:42:30 -0400

    test: fix get href command

--- cypress/e2e/add-procuct-to-cart.cy.ts
+++ cypress/e2e/add-procuct-to-cart.cy.ts
@@ -1,8 +1,8 @@
-describe('add prodict to cart', () => {
+describe('add product to cart', () => {
   it('should be able to navigate to the product page and add it to the cart', () => {
     cy.visit('http://localhost:3000')
 
-    cy.get('a[href⁼"/product"]').first().click()
+    cy.get('a[href^="/product"]').first().click()
 
     // cy.url().should('include', '/product')
     cy.location('pathname').should('include', '/product')
@@ -14,7 +14,7 @@ describe('add prodict to cart', () => {
   it('should not count duplicated products on cart', () => {
     cy.visit('http://localhost:3000')
 
-    cy.get('a[href⁼"/product"]').first().click()
+    cy.get('a[href^="/product"]').first().click()
 
     // cy.url().should('include', '/product')
     cy.location('pathname').should('include', '/product')
@@ -28,7 +28,7 @@ describe('add prodict to cart', () => {
     cy.visit('http://localhost:3000')
 
     cy.get('input[name=q]').type('moletom').parent('form').submit()
-    cy.get('a[href⁼"/product"]').first().click()
+    cy.get('a[href^="/product"]').first().click()
     cy.location('pathname').should('include', '/product')
     cy.contains('Adicionar ao carrinho').click()
     cy.contains('Cart (1)').should('exist')
